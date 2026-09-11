affbc0eaad946891fbbe73c50b3b751fa1a557f1
ilya.kosolapov <ilya.kosolapov@rt.ru>
2026-09-11 16:29:42 +0300

    Калькулятор | добавлен t.Parallel()

--- internal/base/calcalator_test.go
+++ internal/base/calcalator_test.go
@@ -1,16 +1,20 @@
 package base_test
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"job4j.ru/go-lang-base/internal/base"
-	"testing"
 )
 
 func Test_Add(t *testing.T) {
+	t.Parallel()
 
-	rsl := base.Add(1, 2)
-	expected := 3
+	t.Run("1 + 2 = 3", func(t *testing.T) {
+		t.Parallel()
+		rsl := base.Add(1, 2)
+		expected := 3
 
-	assert.Equal(t, rsl, expected)
-
+		assert.Equal(t, rsl, expected)
+	})
 }
